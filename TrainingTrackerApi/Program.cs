d16528e1f8fa3ba4548fe82305668bce88827a3b
Elvis Nilsson <Nilssonelvis@gmail.com>
2026-02-20 13:17:13 +0100

    Changes

--- TrainingTrackerApi/Program.cs
+++ TrainingTrackerApi/Program.cs
@@ -30,7 +30,7 @@ app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "TrainingTracker API v1");
-    options.RoutePrefix = string.Empty;
+    options.RoutePrefix = "swagger";
 });
 
 app.UseHttpsRedirection();

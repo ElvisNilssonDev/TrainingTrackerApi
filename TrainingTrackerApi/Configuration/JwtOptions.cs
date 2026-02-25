d9c1d1ff8a6ca5e7f90a24a17f31fa3c95275221
Elvis Nilsson <Nilssonelvis@gmail.com>
2026-02-25 22:26:42 +0100

    Got done with two classes for Authentication

---
+++ TrainingTrackerApi/Configuration/JwtOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace TrainingTrackerApi.Configuration
+{
+    public class JwtOptions
+    {
+        public const string SectionName = "Jwt";
+
+        public required string Issuer { get; init; }
+        public required string Audience { get; init; }
+        public required string SigningKey { get; init; }
+        public int ExpirationMinutes { get; init; } = 60;
+    }
+}
